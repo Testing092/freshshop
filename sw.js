dd2c080af00d8a9a1f338494385dc7b3720e4e04
Sandeep S <39721019+codebeginner092@users.noreply.github.com>
2020-08-13 18:43:14 +0530

    images added

---
+++ sw.js
@@ -0,0 +1,32 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+    "https://i.imgur.com/Kbkqr2n.png",
+    "https://i.imgur.com/lgLaG0x.png",
+    "https://i.imgur.com/0iL8mxP.png",
+    "https://i.imgur.com/KDsdYeS.png",
+    "https://i.imgur.com/mK50fqL.png",
+    "https://i.imgur.com/KYGH2Qa.png"
+  
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
